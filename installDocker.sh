ef1557209970a01ab56378822c8e4f516c369951
Grisson Wang <grissonz@microsoft.com>
2017-08-03 10:34:56 -0700

    add option to systemctl

--- installDocker.sh
+++ installDocker.sh
@@ -5,4 +5,4 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
-sudo systemctl status docker
+sudo systemctl status docker --no-pager
