5f77518f849258dd2160df4c42fc68d2269b98c5
Grisson Wang <grissonz@microsoft.com>
2017-08-04 10:16:02 -0700

    fix merge

--- installLocust.sh
+++ installLocust.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
-pip install locustio, pyzmq
-=======
 sudo apt-get update
 sudo apt-get install -y python-pip
-pip install locustio pyzmq
->>>>>>> 271eeb167d0f0a4157e65d2d0f681c507cf53e03
+pip install -y locustio, pyzmq
+
