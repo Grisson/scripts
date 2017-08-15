c6b42e9f48627c05db60e5f15d8b1cfc3a430a82
Grisson Wang <grissonz@microsoft.com>
2017-08-15 11:37:21 -0700

    add new docker image and repository

--- docker/prebidServerQuickStart.sh
+++ docker/prebidServerQuickStart.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker run -d -p 80:8000 grissonz/prebid-server
+#sudo docker login mediationhubint.azurecr.io -u MediationHubInt  -p v==/W/gcR/kH+vgQ=xzT=+Ro=mppT3mr 
+
+#sudo docker run -d -p 80:8000 grissonz/prebid-server
+
+sudo docker run -d -p 80:8000 mediationhubint.azurecr.io/grissonz/prebid-server-withbing
