8ad94816f959ec4a4ee0affa5832b0971f2f34cb
Grisson Wang <grisson@gmail.com>
2017-08-27 11:15:27 -0700

    rename and add install AZ cli script

---
+++ myUbuntu/installAZ.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "deb https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
+
