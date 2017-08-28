39b8fffa38e10fdf9bf804a7d665048bffc8fc03
Grisson Wang <grissonz@microsoft.com>
2017-08-27 22:04:05 -0700

    working on new script for front door

--- acs.sh
+++ acs.sh
@@ -4,9 +4,15 @@ az group create --name PACSG --location westus
 
 acs create --orchestrator-type=kubernetes --resource-group=PACSG --name=k8C1 --generate-ssh-keys --agent-count=2 --agent-vm-size=Standard_DS4_v2  --location westus
 
+sudo az acs kubernetes install-cli
+
 acs kubernetes get-credentials --resource-group=PACSG --name=k8C1
+//This command downloads the cluster credentials to $HOME/.kube/config, where kubectl expects it to be located.
+
 
 kubectl get nodes
+kubectl describe pods
+
 
 acr list --resource-group MediationHub --query "[].{acrLoginServer:loginServer}" --output table
 
@@ -14,9 +20,13 @@ acr list --resource-group MediationHub --query "[].{acrLoginServer:loginServer}"
 kubectl create -f ./prebidServer.yaml
 
 kubectl get service prebidserver-svc --watch
+kubectl describe services/kprebidserver-svc
 
 
 kubectl scale --replicas=64 deploy/prebidserver-deployment
+kubectl set image deployments/prebidserver-deployment  prebidserver-deployment= 
+kubectl rollout status deployments/prebidserver-deployment
+kubectl rollout undeo deployments/prebidserver-deployment
 
 
 //
