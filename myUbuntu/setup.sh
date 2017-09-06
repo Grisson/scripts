d1cf11c560579fcb7688be381480130252841446
Grisson Wang <Grisson@users.noreply.github.com>
2017-09-05 17:27:15 -0700

    update bash-it script

--- myUbuntu/setup.sh
+++ myUbuntu/setup.sh
@@ -2,5 +2,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 
+~/.bash_it/install --slient
+
+car >> ~/.bashrc
+cat "[[ $TERM != "screen" ]] && exec tmux" >> ~/.bashrc
 
 cp ./vimrc ~/.vimrc
