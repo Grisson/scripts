3196fea47ae6e77b037dbead9930d7760ffd36f7
Grisson(Zhaomin) Wang <Grisson@users.noreply.github.com>
2021-11-14 21:35:54 -0800

    Fix script issue
    fix the vim plug installation and tmux.config url

--- mac/setup.sh
+++ mac/setup.sh
@@ -11,12 +11,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cp zshrc ~/.zshrc
 
 # install vim plug
-curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrc ~/.vimrc
 # run the plug install command
 vim +PluginInstall +qall
 
 # install tmux
 brew install tmux
-curl -fLo ~/.tmux.conf https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.tmux.conf https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf
 cp tmux.conf.local ~/.tmux.conf.local
