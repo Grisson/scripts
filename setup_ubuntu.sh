2358dfbec9185b57e80b529026973c17358245ea
Grisson Wang <grisson@gmail.com>
2022-11-15 23:14:09 -0800

    add ros set up

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -90,3 +90,24 @@ conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvi
 conda deactivate
 
 # python3 -c "import torch; print(torch.cuda.is_available())"
+
+
+#
+# ROS
+#
+# humble
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl gnupg lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt install ros-humble-desktop
+
+conda create --name ros python=3.10
+conda activate ros
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'source /opt/ros/humble/setup.bash' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+# echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+
+
+# source /opt/ros/humble/setup.bash
